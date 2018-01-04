9717f100a747d21d3e7842e124576d1afdb59272
Jacob Pelletier <xuofmx@gmail.com>
2018-01-04 18:35:04 -0500

    Batch with comments added

--- BLOCKALL.bat
+++ BLOCKALL.bat
@@ -1,11 +1,9 @@
 @ setlocal enableextensions  //This ensures that environment changes made are local to the batch file and restored upon exit.
 @ cd /d "%~dp0"
 
-
-for /R %%a in (*.exe) do ( //Initializing process for the functions below.
-
-//If you only want inbound or outbound blocked and not both delete the corresponding line below.
+//Initializing process for the functions below.
+//If you only want inbound or outbound blocked delete the corresponding line below.
+for /R %%a in (*.exe) do (
 netsh advfirewall firewall add rule name="Blocked with Batchfile %%a" dir=in program="%%a" action=block //inbound rules
-
 netsh advfirewall firewall add rule name="Blocked with Batchfile %%a" dir=out program="%%a" action=block //outbound rules
 )
